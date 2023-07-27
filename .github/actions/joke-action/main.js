dcdd93e365607d53a40bce7685cbc4881cd52aaa
Dan Hardej <danielhardej@github.com>
2023-07-27 11:33:01 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -22,9 +22,9 @@ async function run() {
   core.setOutput("joke-output", joke);
 
   await octokit.rest.issues.createComment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.issue.number,
+    // owner: context.repo.owner,
+    // repo: context.repo.repo,
+    // issue_number: context.issue.number,
     body: 'Thank you for adding a label! We will try to review this as soon as we can.'
   });
 }
