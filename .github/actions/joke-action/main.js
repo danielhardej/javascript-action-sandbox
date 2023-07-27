4ef0e4a431685e96491eab753ecb063fd6c85fa5
Dan Hardej <danielhardej@github.com>
2023-07-27 11:41:39 +0800

    Update workflow

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -14,6 +14,9 @@ const { context } = github.context;
 const token = core.getInput('GITHUB_TOKEN');
 // const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 const octokit = new github.getOctokit(token);
+const owner = core.getInput('owner');
+const repo = core.getInput('repo');
+const issue_number = core.getInput('issue_number');
 
 async function run() {
 
@@ -22,10 +25,10 @@ async function run() {
   core.setOutput("joke-output", joke);
 
   await octokit.rest.issues.createComment({
-    // owner: context.repo.owner,
-    repo: context.repo,
-    issue_number: issue.number,
-    body: 'Thank you for adding a label! We will try to review this as soon as we can.'
+    owner,
+    repo,
+    issue_number,
+    body: `Thank you for adding a label! We will try to review this as soon as we can.`
   });
 }
 
